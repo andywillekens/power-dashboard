bfdc53744100812c93ab30eeb16975f0b832133d
Andy @ Personal <andywillekens@gmail.com>
2025-10-09 10:36:47 +0200

    Installed tailwind

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,18 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/icon']
+  css: ['~/assets/css/main.css'],
+  modules: ['@nuxt/fonts', '@nuxt/icon'],
+  vite: { plugins: [tailwindcss()] },
+  runtimeConfig: {
+    apiUrl: process.env.API_URL,
+    apSystemsUserId: process.env.APSYSTEMS_USER_ID,
+    toonAccessToken: process.env.TOON_ACCESS_TOKEN,
+    public: {
+      apiUrl: process.env.API_URL
+    }
+  }
 })
