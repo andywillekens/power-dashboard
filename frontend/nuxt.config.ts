802bf502597bba78ce4e2fbf4f6389abf4623498
Andy @ Personal <andywillekens@gmail.com>
2025-10-13 10:05:44 +0200

    Added favicon

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -7,6 +7,17 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   modules: ['@nuxt/fonts', '@nuxt/icon'],
   vite: { plugins: [tailwindcss()] },
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/svg+xml',
+          href: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🔋</text></svg>'
+        }
+      ]
+    }
+  },
   runtimeConfig: {
     apiUrl: process.env.API_URL,
     apSystemsUserId: process.env.APSYSTEMS_USER_ID,
